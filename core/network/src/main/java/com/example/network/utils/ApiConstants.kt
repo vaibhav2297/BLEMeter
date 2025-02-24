909352e773402b709cd7433fc26da2878aa3b81c
Vaibhav2209 <vaibhavpatel221997@gmail.com>
2025-02-24 09:54:48 +0530

    🐛 : Auth token and refresh token has the same storage key string

--- core/network/src/main/java/com/example/network/utils/ApiConstants.kt
+++ core/network/src/main/java/com/example/network/utils/ApiConstants.kt
@@ -8,5 +8,6 @@ internal object ApiConstants {
         const val WALLET_END_POINT = "/rest/v1/wallet"
         const val SIGNUP_END_POINT = "/auth/v1/signup"
         const val LOGIN_END_POINT = "/auth/v1/token"
+        const val METER_TRANSACTION_END_POINT = "/rest/v1/meter_transactions"
     }
 }
